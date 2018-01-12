84553cd6aa2389ad31fabdc5330c8bd98270db05
Hamza <makraz.hamza@gmail.com>
2018-01-12 01:50:35 +0000

    front end work create demo app second revision

--- demo/static/js/main.js
+++ demo/static/js/main.js
@@ -1,14 +1,3 @@
 setTimeout(function () {
     $('.loader-wrap').fadeOut();
 }, 1200);
-
-var count = 30;
-
-function timer() {
-    count = count - 1;
-    if (count < 0) {clearInterval(counter);return;}
-
-    document.getElementById("timer").innerHTML = count; // watch for spelling
-
-    if (count == 0){location.href = 'check.html';}
-}

798914ee566b9be9e983a0e6341b1815cca27f9d
Yuelin Du <yuelindu@Yuelins-MacBook-Air.local>
2018-01-13 17:34:26 -0800

    add admin for userprofile

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from django.contrib import admin
+from .models import UserProfile, PersonalUserProfile, CompanyUserProfile
+
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(PersonalUserProfile)
+admin.site.register(CompanyUserProfile)
